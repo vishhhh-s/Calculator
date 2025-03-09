346a9a6fd2d7761aa2cdc05da943ab3a814104e2
Maria <wmasha2006@gmail.com>
2025-03-09 17:56:30 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1 число");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Вветиде операцию");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Введите 2 число");
+            double b = Convert.ToDouble(Console.ReadLine());
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(a+b);
+                    break;
+                case "-":
+                    Console.WriteLine(a-b);
+                    break;
+                case "/":
+                    if(b!=0)
+                    Console.WriteLine(a/b);
+                    else
+                        Console.WriteLine("Ошибка");
+                        break;
+                case "*":
+                    Console.WriteLine(a *b);
+                    break;
+                case "%":
+                    Console.WriteLine(a%b);
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная операция");
+                    break;
+            }
+        }
+    }
+}
